0853c938bfa83f060fc70c04ade12d372a11c54b
Ali Nasir <47711616+alinasir85@users.noreply.github.com>
2021-08-19 00:29:04 +0500

    Updates

---
+++ precache-manifest.9bdce70c03bded73a0642ffbba7bbe83.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d30139addcecd08ffa66cd2691b81d4",
+    "url": "/clothingVibe/index.html"
+  },
+  {
+    "revision": "e0116fb5226f8e3238a0",
+    "url": "/clothingVibe/static/css/main.cc3c6006.chunk.css"
+  },
+  {
+    "revision": "dc2bbb9689d15750c5fb",
+    "url": "/clothingVibe/static/js/2.44c977c9.chunk.js"
+  },
+  {
+    "revision": "b01cf20ee08477db845d02c3b279da13",
+    "url": "/clothingVibe/static/js/2.44c977c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0116fb5226f8e3238a0",
+    "url": "/clothingVibe/static/js/main.1495f8ca.chunk.js"
+  },
+  {
+    "revision": "50b60ca227a74595f13d",
+    "url": "/clothingVibe/static/js/runtime-main.62f29b6d.js"
+  },
+  {
+    "revision": "556ed7c6582ea69639fe8903fad74884",
+    "url": "/clothingVibe/static/media/crown.556ed7c6.svg"
+  },
+  {
+    "revision": "cf7c11da0afb721295842877e1918145",
+    "url": "/clothingVibe/static/media/crown.cf7c11da.png"
+  },
+  {
+    "revision": "09e6f5e77390c30cbf9590bd0022c883",
+    "url": "/clothingVibe/static/media/shopping-bag.09e6f5e7.svg"
+  }
+]);
